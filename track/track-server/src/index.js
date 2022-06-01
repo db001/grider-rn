f37e2016f19bab3c08cd103e7077e49202df2211
Dave <db001@users.noreply.github.com>
2022-06-01 16:26:34 +0100

    WIP

--- track/track-server/src/index.js
+++ track/track-server/src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 require("dotenv").config();
 
@@ -23,8 +24,8 @@ mongoose.connection.on("error", (err) => {
 	console.log("Mongo connection error", err);
 });
 
-app.get("/", (req, res) => {
-	res.send("Hi there!");
+app.get("/", requireAuth, (req, res) => {
+	res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
